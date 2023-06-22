09267bdd7a84f122a15354e3a38529874083148a
Sanari Wickramaratne <sanariwickramaratne@Sanaris-MacBook-Air.local>
2023-06-22 11:41:51 -0400

    Commit

--- Assignment_3.R
+++ Assignment_3.R
@@ -11,14 +11,14 @@ secret_word <- tolower(sample(wordlist, 1))
 word_length <- nchar(secret_word)
 
 # Set the maximum number of wrong guesses
-max_wrong_guesses <- 20
+max_wrong_guesses <- 10
 
 # Initialize variables to store guessed letters during game and the number of wrong guesses.
 guessed_letters <- character(0)
 wrong_guesses <- 0
 
 # Function to display the current state of the game
-displayState <- function(secret_word, guessed_letters, wrong_guesses, max_wrong_guesses) {
+display_state <- function(secret_word, guessed_letters, wrong_guesses, max_wrong_guesses) {
   revealed_word <- ""
   for (letter in strsplit(secret_word, "")[[1]]) {
     if (letter %in% guessed_letters) {
@@ -33,30 +33,34 @@ displayState <- function(secret_word, guessed_letters, wrong_guesses, max_wrong_
 }
 
 # Function to check if the input is a valid letter
-isValidLetter <- function(input) {
+valid_letter <- function(input) {
   grepl("^[a-zA-Z]$", input)
 }
 
-# Main game loop
-cat("Welcome to Hangman!\n")
-cat("The word has", word_length, "letters.\n")
+# Introduction to game
+cat("Welcome to Sanari's Hangman Game! I hope you enjoy playing! \n")
+cat("The word has", word_length, "letters. You are allowed 20 guesses in total. Good luck!!\n")
 
+# Create a while loop to prompt user to enter a letter until the game is over.
 while (wrong_guesses < max_wrong_guesses) {
-  displayState(secret_word, guessed_letters, wrong_guesses, max_wrong_guesses)
+  display_state(secret_word, guessed_letters, wrong_guesses, max_wrong_guesses)
 
-  # Prompt user for input
-  guess <- readline("Enter your guess: ")
+  # Prompt user to enter a letter
+  guess <- readline("Please enter a letter: ")
   guess <- tolower(guess)
 
   # Check if the input is a valid letter
-  if (!isValidLetter(guess)) {
+  if (!isvalid_letter(guess)) {
     cat("Please enter a valid letter.\n")
     next
   }
 
+  # Converting the guess to lowercase
+  guess <- tolower(guess)
+
   # Check if the letter has already been guessed
   if (guess %in% guessed_letters) {
-    cat("You already guessed that letter.\n")
+    cat("You already guessed that letter. Please try again. \n")
     next
   }
 
@@ -73,15 +77,16 @@ while (wrong_guesses < max_wrong_guesses) {
 
   # Check if the user has guessed all the letters
   if (all(strsplit(secret_word, "")[[1]] %in% guessed_letters)) {
-    displayState(secret_word, guessed_letters, wrong_guesses, max_wrong_guesses)
-    cat("Congratulations! You won!\n")
+    display_state(secret_word, guessed_letters, wrong_guesses, max_wrong_guesses)
+    cat("Congratulations! You have successfully guessed the secret word! YAY!! \n")
     break
   }
 }
 
-# If the user has used all the guesses
+  # If the user has used up all the guesses, let them know that they have lost the game
 if (wrong_guesses == max_wrong_guesses) {
-  displayState(secret_word, guessed_letters, wrong_guesses, max_wrong_guesses)
-  cat("Sorry, you lost. The word was:", secret_word, "\n")
+  display_state(secret_word, guessed_letters, wrong_guesses, max_wrong_guesses)
+  cat("Oh no! You lost the game! The word was:", secret_word, "\n")
 }
 
+# END OF GAME
