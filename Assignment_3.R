e617286d6a415ae718e819c211117ea14b8c6531
SanariW <135373691+SanariW@users.noreply.github.com>
2023-06-22 17:22:17 -0400

    Assignment_3.R

--- Assignment_3.R
+++ Assignment_3.R
@@ -10,8 +10,8 @@ get_secret_word <- function(wordlist) {
 }
 
 # Function to check if the user input is a valid letter
-valid_letter <- function(input) {
-  grepl("^[a-zA-Z]$", input)
+valid_letter <- function(user_input) {
+  grepl("^[a-zA-Z]$", user_input)
 }
 
 # Function to check if the user input is a valid command incase they want to type the whole word, restart the game or exit the game. 
@@ -35,7 +35,7 @@ reset_game <- function() {
 }
 
 # Visually display the current state of the game for the user
-display_state <- function() {
+display_state <- function(secret_word, guessed_letters, wrong_guesses, max_wrong_guesses) {
   revealed_word <- ""
   for (letter in strsplit(secret_word, "")[[1]]) {
     if (letter %in% guessed_letters) {
@@ -56,32 +56,40 @@ game_over <- FALSE
 cat("Welcome to Sanari's Hangman Game! The category is cities within Ontario, Canada. I hope you enjoy playing! \n")
 cat("You are allowed 10 guesses in total. If you would like to guess the whole word, simply type 'word'. If you would like to restart the game, type 'restart'. If you would like to exit the game, type 'exit'. Good luck!!\n")
 
+# Set the maximum number of wrong guesses
+max_wrong_guesses <- 10
+
 # Reset the game initially
 reset_game()
 
 # Create a while loop to prompt user to enter a letter until the game is over or until they exit, or restart the game
+# Note: the user can only play the game once if they have successfully won. 
 while (!game_over) {
-  display_state()
+  display_state(secret_word, guessed_letters, wrong_guesses, max_wrong_guesses)
   
   # Prompt user to enter a letter or command
   guess <- tolower(readline("Please enter a letter or command (word/restart/exit): "))
+ 
   # Check if the input is a valid command
   if (valid_command(guess)) {
     if (guess == "word") {
+  
   # Prompt user to enter the entire word
       guessed_word <- tolower(readline("Enter the entire word: "))
       if (guessed_word == secret_word) {
-        display_state()
+        display_state(secret_word, guessed_letters, wrong_guesses, max_wrong_guesses)
         cat("Congratulations! You have successfully guessed the secret word! YAY!! \n")
       } else {
         cat("Wrong word! Oh no! You lost the game. The word was:", secret_word, "\n")
       }
       game_over <- TRUE
     } else if (guess == "restart") {
+  
   # Reset the game
       reset_game()
       cat("Game restarted. Good luck!!\n")
     } else if (guess == "exit") {
+  
   # Exit the game
       cat("Exiting the game. Hope you had fun! Goodbye!\n")
       game_over <- TRUE
@@ -114,11 +122,10 @@ while (!game_over) {
   
   # Check if the user has guessed all the letters
   if (all(strsplit(secret_word, "")[[1]] %in% guessed_letters)) {
-    display_state()
+    display_state(secret_word, guessed_letters, wrong_guesses, max_wrong_guesses)
     cat("Congratulations! You have successfully guessed the secret word! YAY!! \n")
     game_over <- TRUE
   }
 }
 
-# Note: the user can only play the game once if they have successfully won. 
 # END OF GAME
